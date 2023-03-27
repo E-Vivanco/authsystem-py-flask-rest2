360aab715d766c7caf2c0d08b870fdefbfde9cf9
E-Vivanco <vivanco.elsa@gmail.com>
2023-03-27 06:44:23 +0000

    jwt config

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
-import os
 from flask import Flask, request, jsonify, render_template
+import os
 from flask_migrate import Migrate
 from flask_cors import CORS
 from dotenv import load_dotenv
-from models import db
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from werkzeug.security import generate_password_hash,check_password_hash
+from models import db, User
 
 load_dotenv()
 
@@ -13,14 +15,65 @@ app.config['DEBUG']= True
 app.config['ENV']= 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URI')
+app.config['JWT_SECRET_KEY']='b90e47152bba9fe60f4fceef21ea4827'
 
 db.init_app(app)
 Migrate(app, db)
 CORS(app)
+jwt=JWTManager(app)
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    name =request.json.get('name','')
+    email=request.json.get('email')
+    password=request.json.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user :
+        return jsonify({"msg": "usuario ya se encuentra registrado"})
+
+    user = User() 
+    user.name= name
+    user.email= email
+    user.password = generate_password_hash(password)
+    user.save()
+
+    return jsonify({"msg": "User registrado"}), 200
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    user = User.query.filter_by(email = email).first()
+    if not user:
+        return jsonify({"msg":"El usuario o la pass no coinciden"}), 400
+    if not check_password_hash(user.password,password):
+        return jsonify({"msg":"El user o la pass no coinciden"}),400
+    access_token = create_access_token(identity= user.email)
+
+    data={
+            "access_token": access_token,
+            "user": user.serialize()
+        }
+    return jsonify(data)
+
+@app.route('/api/user', methods=['GET'])
+@jwt_required()
+def user():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(),users))
+
+    return jsonify(users), 200
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db= SQLAlchemy()
 class User(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password =db.Column(db.String, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password =db.Column(db.String(100), nullable=False)
+    isActive = db.Column(db.Boolean(),default=True)
 
 
     def serialize(self):
@@ -14,7 +15,9 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "email": self.email,
-            "password": self.password
+            "password": self.password,
+            "isActive": self.isActive
+
         }
     
     def save(self):
